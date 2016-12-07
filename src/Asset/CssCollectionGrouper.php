a58c5e5db69daf647bdba4a52c163a0d1d610a9c
chriskinch <kinchie@gmail.com>
2016-12-07 18:03:59 +0000

    Added const for the inline group

--- src/Asset/CssCollectionGrouper.php
+++ src/Asset/CssCollectionGrouper.php
@@ -5,6 +5,11 @@ namespace Drupal\css_inline\Asset;
 use Drupal\Core\Asset\CssCollectionGrouper as CoreCssCollectionGrouper;
 
 /**
+ * The default aggregation group for inline CSS files added to the page.
+ */
+const CSS_AGGREGATE_INLINE = 200;
+
+/**
  * Groups CSS assets.
  */
 class CssCollectionGrouper extends CoreCssCollectionGrouper {
@@ -16,7 +21,7 @@ class CssCollectionGrouper extends CoreCssCollectionGrouper {
     // Alter inline group so that it isn't aggregated with regular files.
     foreach ($css_assets as &$item) {
       if (!empty($item['inline'])) {
-        $item['group'] = $item['group'] - 1;
+        $item['group'] = CSS_AGGREGATE_INLINE;
       }
     }
     return parent::group($css_assets);
